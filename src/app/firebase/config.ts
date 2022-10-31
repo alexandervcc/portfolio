8a5d5d71a9a85e5da40076335c9202654e97dd4d
Alexander Cuenca <alex.charco.25@gmail.com>
2022-10-31 00:09:06 -0500

    Firebase config

---
+++ src/app/firebase/config.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAR55NDC__G-av6TIlfK-p9h4AAUFNCoxQ',
+  authDomain: 'acc-portfolio-41c30.firebaseapp.com',
+  projectId: 'acc-portfolio-41c30',
+  storageBucket: 'acc-portfolio-41c30.appspot.com',
+  messagingSenderId: '599263517336',
+  appId: '1:599263517336:web:3b6efffa2828e4189dceed',
+  measurementId: 'G-R526CR4FVY',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db };
