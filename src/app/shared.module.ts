396bada06bea3c68b9d0d07d401293825e4b205e
Alexander Cuenca <alex.charco.25@gmail.com>
2022-11-25 23:38:27 -0500

    Pipe added to format Work Experience Date

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DateFormatterPipe } from './shared/pipes/date-formatter.pipe';
 
 @NgModule({
+  declarations: [DateFormatterPipe],
   imports: [CommonModule, BrowserModule, NgbModule],
-  exports: [CommonModule, BrowserModule, NgbModule],
+  exports: [CommonModule, BrowserModule, NgbModule, DateFormatterPipe],
 })
 export class SharedModule {}
--- src/app/shared/pipes/date-formatter.pipe.ts
+++ src/app/shared/pipes/date-formatter.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { MONTHS } from 'src/app/constants/months';
 
 @Pipe({
-  name: 'dateFormatter'
+  name: 'workDateFormat',
 })
 export class DateFormatterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value?: Date, ...args: unknown[]): string {
+    if (!value) {
+      return 'Present';
+    }
+    const timeValue = value as any;
+    const date = new Date(timeValue.seconds * 1000);
+    return date.getFullYear() + ' ' + MONTHS[date.getMonth()];
   }
-
 }
