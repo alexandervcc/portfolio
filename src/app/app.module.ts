b7ba988ae4e0599a863ec15389166d8a1c4075ee
Alexander Cuenca <alex.charco.25@gmail.com>
2022-12-25 22:07:14 -0500

    Project routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ProjectComponent } from './pages/project/project.component';
+import { ProjectComponent } from './pages/projects/project.component';
 import { HomeModule } from './pages/home/home.module';
 import { SharedModule } from './shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AppRouterModule } from './app-router.module';
+import { SingleProjectComponent } from './pages/single-project/single-project.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { AppRouterModule } from './app-router.module';
     NavbarComponent,
     FooterComponent,
     ProjectComponent,
+    SingleProjectComponent,
     NotFoundComponent,
   ],
   imports: [
