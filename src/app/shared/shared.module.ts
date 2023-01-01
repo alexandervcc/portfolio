2dd0cb9fdcafdf323f0e7027d881c114b5de28f8
Alexander Cuenca <alex.charco.25@gmail.com>
2022-12-31 19:12:12 -0500

    Dropdown Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,15 @@ import { RouterModule } from '@angular/router';
 import { DateFormatterPipe } from './pipes/date-formatter.pipe';
 import { ChipComponent } from './components/chip/chip.component';
 import { LoadingCircleComponent } from './components/loading-circle/loading-circle.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [DateFormatterPipe, ChipComponent, LoadingCircleComponent],
+  declarations: [
+    DateFormatterPipe,
+    ChipComponent,
+    LoadingCircleComponent,
+    DropdownComponent,
+  ],
   imports: [CommonModule, BrowserModule, RouterModule],
   exports: [
     CommonModule,
@@ -16,6 +22,7 @@ import { LoadingCircleComponent } from './components/loading-circle/loading-circ
     RouterModule,
     ChipComponent,
     LoadingCircleComponent,
+    DropdownComponent,
   ],
 })
 export class SharedModule {}
