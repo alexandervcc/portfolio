67dafd32f445ce513f7411550b6d774d4e6b2795
alexandervcc <alex.charco.25@gmail.com>
2023-09-22 14:23:33 -0500

    Hotjar test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router, RouterState } from '@angular/router';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterState,
+} from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
@@ -8,22 +13,24 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-
   constructor(private router: Router) {
-    this.router.events.subscribe(event => {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
+        const title = this.getTitle(
+          this.router.routerState,
+          this.router.routerState.root
+        ).join('-');
         gtag('event', 'page_view', {
           page_title: title,
           page_path: event.urlAfterRedirects,
-          page_location: document.location.href
+          page_location: document.location.href,
         });
       }
-    })
+    });
   }
 
   ngOnInit() {
-    const head = document.getElementsByTagName('head')[0]
+    const head = document.getElementsByTagName('head')[0];
 
     const scriptDownloadGA = document.createElement('script');
     scriptDownloadGA.src = `https://www.googletagmanager.com/gtag/js?id=${environment.ga}`;
@@ -32,10 +39,13 @@ export class AppComponent implements OnInit {
     const scriptConfigureGA = document.createElement('script');
     scriptConfigureGA.innerHTML = `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "${environment.ga}");`;
 
+    const scriptConfigureHotjar = document.createElement('script');
+    scriptConfigureHotjar.innerHTML = `<script>(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3661076,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');</script>`;
+
     head.insertBefore(scriptDownloadGA, head.firstChild);
     head.insertBefore(scriptConfigureGA, head.firstChild);
+    head.insertBefore(scriptConfigureHotjar, head.firstChild);
   }
-
 
   private getTitle(state: RouterState, parent: ActivatedRoute): string[] {
     const data = [];
